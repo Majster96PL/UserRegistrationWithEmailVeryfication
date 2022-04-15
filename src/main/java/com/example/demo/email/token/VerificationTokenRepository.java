8c7da761d05b65354085acc205602ab72814589b
Majster96PL <piotruniok96@gmail.com>
2022-04-15 23:24:35 +0200

    Add update method to repository

--- src/main/java/com/example/demo/email/token/VerificationTokenRepository.java
+++ src/main/java/com/example/demo/email/token/VerificationTokenRepository.java
@@ -1,11 +1,22 @@
 package com.example.demo.email.token;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Repository
 public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long> {
     Optional<VerificationToken> findByToken(String token);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE ConfirmationToken t " +
+            "SET t.confirmedToken = ?2" +
+            "WHERE t.token = ?1")
+    int updateConfirmedToken(String token, LocalDateTime confirmedToken);
 }
