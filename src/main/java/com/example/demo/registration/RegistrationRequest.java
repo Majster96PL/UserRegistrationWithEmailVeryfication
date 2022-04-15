95c80971c07b2d64cc52c2d064c4432fcf9e5401
Majster96PL <piotruniok96@gmail.com>
2022-04-15 13:05:42 +0200

    Create RegistrationRequest

---
+++ src/main/java/com/example/demo/registration/RegistrationRequest.java
@@ -0,0 +1,8 @@
+package com.example.demo.registration;
+
+public class RegistrationRequest {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+}
