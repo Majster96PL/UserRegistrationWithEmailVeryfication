e7e2481a62445a0619b1545b08fa1c33c30fd496
Majster96PL <piotruniok96@gmail.com>
2022-04-15 21:05:10 +0200

    Add condition and return new user to method

--- src/main/java/com/example/demo/registration/RegistrationService.java
+++ src/main/java/com/example/demo/registration/RegistrationService.java
@@ -1,11 +1,32 @@
 package com.example.demo.registration;
 
+import com.example.demo.email.EmailValidation;
+import com.example.demo.user.User;
+import com.example.demo.user.UserRole;
+import com.example.demo.user.UserService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class RegistrationService {
 
+    private final EmailValidation emailValidation;
+    private final UserService userService;
+
     public String getRegisterUser(RegistrationRequest request) {
-        return "działa czy nie?";
+        boolean isEmailValidation = emailValidation.test(request.getEmail());
+        if(!isEmailValidation){
+            throw new IllegalStateException("Email is not validation.");
+        }
+        return userService.recordUser(
+                new User(
+                        request.getFirstName(),
+                        request.getLastName(),
+                        request.getEmail(),
+                        request.getPassword(),
+                        UserRole.USER
+                )
+        );
     }
 }
