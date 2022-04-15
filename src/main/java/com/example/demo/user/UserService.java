20244d10d8dd98e7fd21f39c2d417a06a791216f
Majster96PL <piotruniok96@gmail.com>
2022-04-15 21:07:28 +0200

    create method recordUser

--- src/main/java/com/example/demo/user/UserService.java
+++ src/main/java/com/example/demo/user/UserService.java
@@ -1,5 +1,6 @@
 package com.example.demo.user;
 
+import com.example.demo.security.PasswordEncoder;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -12,6 +13,7 @@ public class UserService implements UserDetailsService {
 
     private static final String MESSAGE = "User with email %s not found";
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
@@ -19,5 +21,18 @@ public class UserService implements UserDetailsService {
                 .orElseThrow(() -> new UsernameNotFoundException(
                         String.format(MESSAGE, email))
         );
+    }
+
+    public String recordUser(User user){
+        boolean isUserExists = userRepository.findByEmail(user.getEmail()).isPresent();
+        if (isUserExists){
+            throw new IllegalStateException("Email already taken");
+        }
+        String passwordEncoded = passwordEncoder
+                .bCryptPasswordEncoder()
+                .encode(user.getPassword());
+        user.setPassword(passwordEncoded);
+        userRepository.save(user);
+        return " ";
     }
 }
