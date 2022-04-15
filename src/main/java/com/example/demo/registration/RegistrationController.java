bd12893248000f4f7a8b9d62afeac8d7abbdc7cd
Majster96PL <piotruniok96@gmail.com>
2022-04-15 15:10:01 +0200

    Add constructor with parameters in the form a annotation from lombok library and add PostMapping to method

--- src/main/java/com/example/demo/registration/RegistrationController.java
+++ src/main/java/com/example/demo/registration/RegistrationController.java
@@ -1,15 +1,19 @@
 package com.example.demo.registration;
 
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "login/v1/registration")
+@RequestMapping(path = "/login/v1/registration")
+@AllArgsConstructor
 public class RegistrationController {
 
-    private static RegistrationService registrationService;
+    private RegistrationService registrationService;
 
+    @PostMapping
     public String getRegisterUser(@RequestBody RegistrationRequest request){
         return registrationService.getRegisterUser(request);
     }
