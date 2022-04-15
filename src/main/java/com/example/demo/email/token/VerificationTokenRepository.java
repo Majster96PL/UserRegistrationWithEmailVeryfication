54db37abac519a5a5064f5fb69f4a33a1f7fa393
Majster96PL <piotruniok96@gmail.com>
2022-04-15 21:40:23 +0200

    Create JpaReposotory for verification token.

---
+++ src/main/java/com/example/demo/email/token/VerificationTokenRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo.email.token;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long> {
+    Optional<VerificationToken> findByToken(String token);
+}
