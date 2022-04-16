c997b9d98e20c4669e4d5e8028191260ba9aefa1
Majster96PL <piotruniok96@gmail.com>
2022-04-16 13:18:22 +0200

    Add method to repo

--- src/main/java/com/example/demo/user/UserRepository.java
+++ src/main/java/com/example/demo/user/UserRepository.java
@@ -1,6 +1,8 @@
 package com.example.demo.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,4 +12,10 @@ import java.util.Optional;
 @Transactional(readOnly = true)
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE User u "+
+            "SET u.enabled = TRUE WHERE u.email = ?1")
+    int enableUser(String email);
 }
