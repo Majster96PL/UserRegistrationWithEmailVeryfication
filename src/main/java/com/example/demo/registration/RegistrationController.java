ced338e6dcb07e8ef7522836a8035b59087ab94b
Majster96PL <piotruniok96@gmail.com>
2022-04-19 14:25:21 +0200

    change path

--- src/main/java/com/example/demo/registration/RegistrationController.java
+++ src/main/java/com/example/demo/registration/RegistrationController.java
@@ -4,7 +4,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(path = "/login/v1/registration")
+@RequestMapping(path = "/registration")
 @AllArgsConstructor
 public class RegistrationController {
 
